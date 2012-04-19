c6326a6f65f1c548cc562dee9fea23d254133392
Julien Pellet <pelletj@gmail.com>
2012-04-20 08:30:16 +1200

    clean output for CSV

--- read_db.rb
+++ read_db.rb
@@ -6,8 +6,8 @@ User.all.each do |user|
   if user.fb_id == nil
     user.destroy
   end
-  puts user.fb_id +","+ user.access_token
+  puts user.fb_id + "," + user.access_token
 
 end
 
-puts User.all.length.to_s + " users"
+# puts User.all.length.to_s + " users"
