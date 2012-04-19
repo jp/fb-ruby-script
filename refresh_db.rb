c70c4dcc048b52a42f11fed82e990383988b0ce7
Julien Pellet <pelletj@gmail.com>
2012-04-19 17:13:06 +1200

    write CSV

--- refresh_db.rb
+++ refresh_db.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby -rubygems
 require 'lib'
 
+CSV_FILE="tokens.csv"
 
 ############ get app token ################
 path = "/oauth/access_token?client_id=#{APP_ID}&client_secret=#{APP_SECRET}&grant_type=client_credentials"
@@ -10,6 +11,14 @@ APP_ACCESS_TOKEN = get(path)
 users_path = URI.escape("/#{APP_ID}/accounts/test-users?#{APP_ACCESS_TOKEN}&limit=500")
 users = JSON.parse(get(users_path))
 
+
+
+if !File.exists?(CSV_FILE)
+  File.delete(CSV_FILE)
+end
+
+f = File.open(CSV_FILE, 'w')
+
 users["data"].each do |u|
   db_user = User.find_by_fb_id(u["id"])
   if db_user
@@ -18,6 +27,8 @@ users["data"].each do |u|
 
     db_user.access_token = u["access_token"]
     db_user.save
+    f.write(u["access_token"]+"\n")
   end
 end
+f.close
 
