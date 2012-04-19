95563211af66db6292a3f0aee08d616945a1968e
Julien Pellet <pelletj@gmail.com>
2012-04-19 16:40:18 +1200

    fix refresh

--- refresh_db.rb
+++ refresh_db.rb
@@ -7,12 +7,14 @@ path = "/oauth/access_token?client_id=#{APP_ID}&client_secret=#{APP_SECRET}&gran
 APP_ACCESS_TOKEN = get(path)
 
 
-users_path = URI.escape("/#{APP_ID}/accounts/test-users?#{APP_ACCESS_TOKEN}")
+users_path = URI.escape("/#{APP_ID}/accounts/test-users?#{APP_ACCESS_TOKEN}&limit=500")
 users = JSON.parse(get(users_path))
 
 users["data"].each do |u|
   db_user = User.find_by_fb_id(u["id"])
-  db_user.access_token = u["access_token"]
-  db_user.save
+  if db_user
+    db_user.access_token = u["access_token"]
+    db_user.save
+  end
 end
 
